385487c6f9e2cfde009af53e926ad8b17db00625
mozzielol <mozzielol@hotmail.com>
2020-03-24 16:04:33 +0000

    distribution difference

--- utils/visualise.py
+++ utils/visualise.py
@@ -5,3 +5,9 @@ import seaborn as sns; sns.set()
 def visulise_scatter(params):
   sns.scatterplot(params[0],params[1])
   plt.show()
+
+
+def visualise_distr(params):
+	for distr in params:
+		sns.kdeplot(distr)
+	plt.show()
